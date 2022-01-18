72479217c81162ce6391c44742764c5b6905bfd0
miguelrincon76 <miangelrincon@hotmail.com>
2022-01-18 18:17:36 -0500

    Incorporate backend

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import router from "./router";
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.use(VueAxios, axios);
-
 Vue.config.productionTip = false;
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 new Vue({
   store,
